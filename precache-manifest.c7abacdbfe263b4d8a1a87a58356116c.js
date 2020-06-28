c01242ada21d51f40a58de8cf55528c358cb404d
taufiq38 <taufiqulislam38@gmail.com>
2020-06-28 10:29:16 +0600

    Updates

---
+++ precache-manifest.c7abacdbfe263b4d8a1a87a58356116c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b745ec424b0c56a0fe0279edf8a2157",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5c4d4400b19919cdcf99",
+    "url": "/static/css/main.aad7bc4f.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c4d4400b19919cdcf99",
+    "url": "/static/js/main.e2000ed6.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
